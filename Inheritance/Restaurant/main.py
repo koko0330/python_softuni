2c3c5c73579063b201911da1284a846377ac8c9b
koko0330 <60879782+koko0330@users.noreply.github.com>
2020-11-25 10:40:56 +0200

    Add files via upload

---
+++ Inheritance/Restaurant/main.py
@@ -0,0 +1,23 @@
+from project.product import Product
+from project.beverage.beverage import Beverage
+from project.food.food import Food
+from project.food.starter import Starter
+from project.food.soup import Soup
+
+
+product = Product("coffee")
+print(product.__class__.__name__)
+print(product.name)
+print(product.price)
+beverage = Beverage("coffee", 2.5, 50)
+print(beverage.__class__.__name__)
+print(beverage.__class__.__bases__[0].__name__)
+print(beverage.name)
+print(beverage.price)
+print(beverage.milliliters)
+soup = Soup("fish soup", 9.90, 230)
+print(soup.__class__.__name__)
+print(soup.__class__.__bases__[0].__name__)
+print(soup.name)
+print(soup.price)
+print(soup.grams)
